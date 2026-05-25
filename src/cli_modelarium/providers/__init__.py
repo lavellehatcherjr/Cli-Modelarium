e398a857617af22516bc0f20cc2cba1eab47b757
Lavelle Hatcher Jr <lavelle@soravantia.com>
2026-05-26 00:33:59 +0900

    Create __init__.py

---
+++ src/cli_modelarium/providers/__init__.py
@@ -0,0 +1,6 @@
+"""Provider abstraction layer for Cli Modelarium."""
+from __future__ import annotations
+
+from cli_modelarium.providers.base import BaseProvider, CompletionResult
+
+__all__ = ["BaseProvider", "CompletionResult"]
